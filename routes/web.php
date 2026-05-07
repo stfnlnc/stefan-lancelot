bc472d3c83fe5c772a58173acfa2d66e97d61bda
Stefan Lancelot <hello@stefanlancelot.com>
2026-05-07 19:31:16 +0200

    remove contact route

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index'])->name('home');
 Route::get('/works', [MainController::class, 'works'])->name('works');
-Route::get('/contact', [MainController::class, 'contact'])->name('contact');
 Route::get('/services', [MainController::class, 'services'])->name('services');
